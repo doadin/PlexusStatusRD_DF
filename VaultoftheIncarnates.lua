c9c0bfbab538a32c7faa7f02be1cf6ae18baa97d
tbkizle <tbkizle@gmail.com>
2022-11-28 17:06:56 -0500

    Initial First Raid Support

---
+++ VaultoftheIncarnates.lua
@@ -0,0 +1,77 @@
+-- local zone = "Vault of the Incarnates"
+local zoneid = 2119
+
+-- "Eranog"
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Eranog")
+GridStatusRaidDebuff:DebuffId(zoneid, 370597, 101, 5, 5, true) --kill-order
+GridStatusRaidDebuff:DebuffId(zoneid, 371059, 102, 5, 5, true) --melting-armor
+GridStatusRaidDebuff:DebuffId(zoneid, 371955, 103, 5, 5, true) --rising-heat
+GridStatusRaidDebuff:DebuffId(zoneid, 370410, 104, 5, 5, true) --pulsing-flames
+
+-- "Terros"
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Terros")
+GridStatusRaidDebuff:DebuffId(zoneid, 376276, 201, 5, 5, true) --concussive-slam
+GridStatusRaidDebuff:DebuffId(zoneid, 382776, 202, 5, 5, true) --awakened-earth
+GridStatusRaidDebuff:DebuffId(zoneid, 381576, 203, 5, 5, true) --seismic-assault
+
+-- "The Primal Council"
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "The Primal Council")
+GridStatusRaidDebuff:DebuffId(zoneid, 371591, 301, 5, 5, true) --frost-tomb
+GridStatusRaidDebuff:DebuffId(zoneid, 371857, 302, 5, 5, true) --shivering-lance
+GridStatusRaidDebuff:DebuffId(zoneid, 371624, 303, 5, 5, true) --conductive-mark
+GridStatusRaidDebuff:DebuffId(zoneid, 372056, 304, 5, 5, true) --crush
+GridStatusRaidDebuff:DebuffId(zoneid, 374792, 305, 5, 5, true) --faultline
+GridStatusRaidDebuff:DebuffId(zoneid, 372027, 306, 5, 5, true) --slashing-blaze
+
+-- "Sennarth, the Cold Breath"
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Sennarth, the Cold Breath")
+GridStatusRaidDebuff:DebuffId(zoneid, 372736, 401, 5, 5, true) --permafrost
+GridStatusRaidDebuff:DebuffId(zoneid, 372648, 402, 5, 5, true) --pervasive-cold
+GridStatusRaidDebuff:DebuffId(zoneid, 373817, 403, 5, 5, true) --chilling-aura
+GridStatusRaidDebuff:DebuffId(zoneid, 372129, 404, 5, 5, true) --web-blast
+GridStatusRaidDebuff:DebuffId(zoneid, 372044, 405, 5, 5, true) --wrapped-in-webs
+GridStatusRaidDebuff:DebuffId(zoneid, 372082, 406, 5, 5, true) --enveloping-webs
+GridStatusRaidDebuff:DebuffId(zoneid, 371976, 407, 5, 5, true) --chilling-blast
+GridStatusRaidDebuff:DebuffId(zoneid, 372030, 408, 5, 5, true) --sticky-webbing
+GridStatusRaidDebuff:DebuffId(zoneid, 372055, 409, 5, 5, true) --icy-ground
+
+-- "Dathea, Ascended"
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "Dathea, Ascended")
+GridStatusRaidDebuff:DebuffId(zoneid, 378095, 501, 5, 5, true) --crushing-atmosphere
+GridStatusRaidDebuff:DebuffId(zoneid, 377819, 502, 5, 5, true) --lingering-slash
+GridStatusRaidDebuff:DebuffId(zoneid, 374900, 503, 5, 5, true) --microburst
+GridStatusRaidDebuff:DebuffId(zoneid, 376802, 504, 5, 5, true) --razor-winds
+GridStatusRaidDebuff:DebuffId(zoneid, 375580, 505, 5, 5, true) --zephyr-slam
+
+-- "Kurog Grimtotem"
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "Kurog Grimtotem")
+GridStatusRaidDebuff:DebuffId(zoneid, 374864, 601, 5, 5, true) --primal-break
+GridStatusRaidDebuff:DebuffId(zoneid, 372158, 602, 5, 5, true) --sundering-strike
+GridStatusRaidDebuff:DebuffId(zoneid, 373681, 603, 5, 5, true) --biting-chill
+GridStatusRaidDebuff:DebuffId(zoneid, 373487, 604, 5, 5, true) --lightning-crash
+GridStatusRaidDebuff:DebuffId(zoneid, 372514, 605, 5, 5, true) --frost-bite
+GridStatusRaidDebuff:DebuffId(zoneid, 372517, 606, 5, 5, true) --frozen-solid
+GridStatusRaidDebuff:DebuffId(zoneid, 377780, 607, 5, 5, true) --skeletal-fractures
+GridStatusRaidDebuff:DebuffId(zoneid, 374623, 608, 5, 5, true) --frost-binds
+GridStatusRaidDebuff:DebuffId(zoneid, 374554, 609, 5, 5, true) --lava-pool
+
+-- "Broodkeeper Diurna"
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "Broodkeeper Diurna")
+GridStatusRaidDebuff:DebuffId(zoneid, 378782, 701, 5, 5, true) --mortal-wounds
+GridStatusRaidDebuff:DebuffId(zoneid, 375829, 702, 5, 5, true) --clutchwatchers-rage
+GridStatusRaidDebuff:DebuffId(zoneid, 378787, 703, 5, 5, true) --crushing-stoneclaws
+GridStatusRaidDebuff:DebuffId(zoneid, 375871, 704, 5, 5, true) --wildfire
+GridStatusRaidDebuff:DebuffId(zoneid, 376266, 705, 5, 5, true) --burrowing-strike
+GridStatusRaidDebuff:DebuffId(zoneid, 375575, 706, 5, 5, true) --flame-sentry
+GridStatusRaidDebuff:DebuffId(zoneid, 375475, 707, 5, 5, true) --rending-bite
+GridStatusRaidDebuff:DebuffId(zoneid, 375457, 708, 5, 5, true) --chilling-tantrum
+GridStatusRaidDebuff:DebuffId(zoneid, 375653, 709, 5, 5, true) --static-jolt
+GridStatusRaidDebuff:DebuffId(zoneid, 376392, 710, 5, 5, true) --disoriented
+GridStatusRaidDebuff:DebuffId(zoneid, 375876, 711, 5, 5, true) --icy-shards
+GridStatusRaidDebuff:DebuffId(zoneid, 375430, 712, 5, 5, true) --sever-tendon
+
+-- "Raszageth the Storm-Eater"
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Raszageth the Storm-Eater")
+GridStatusRaidDebuff:DebuffId(zoneid, 381615, 801, 5, 5, true) --static-charge
+GridStatusRaidDebuff:DebuffId(zoneid, 377594, 802, 5, 5, true) --lightning-breath
+GridStatusRaidDebuff:DebuffId(zoneid, 381249, 803, 5, 5, true) --electrifying-presence
