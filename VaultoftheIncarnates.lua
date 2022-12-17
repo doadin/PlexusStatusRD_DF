59248b4450e7326e05bb0f51a3c8a7988bdabd41
tbkizle <tbkizle@gmail.com>
2022-12-17 02:29:03 -0500

    Big Update to Vault of the Incarnates

--- VaultoftheIncarnates.lua
+++ VaultoftheIncarnates.lua
@@ -7,12 +7,18 @@ GridStatusRaidDebuff:DebuffId(zoneid, 370597, 101, 5, 5, true) --kill-order
 GridStatusRaidDebuff:DebuffId(zoneid, 371059, 102, 5, 5, true) --melting-armor
 GridStatusRaidDebuff:DebuffId(zoneid, 371955, 103, 5, 5, true) --rising-heat
 GridStatusRaidDebuff:DebuffId(zoneid, 370410, 104, 5, 5, true) --pulsing-flames
+GridStatusRaidDebuff:DebuffId(zoneid, 394906, 105, 6, 6, true, true) -- Burning Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 396023, 106, 5, 5, true, true) -- Incinerating Roar
+GridStatusRaidDebuff:DebuffId(zoneid, 390715, 107, 6, 6, true) -- Flamerift
+GridStatusRaidDebuff:DebuffId(zoneid, 370648, 108, 5, 5) -- Lava Flow
 
 -- "Terros"
 GridStatusRaidDebuff:BossNameId(zoneid, 200, "Terros")
 GridStatusRaidDebuff:DebuffId(zoneid, 376276, 201, 5, 5, true) --concussive-slam
 GridStatusRaidDebuff:DebuffId(zoneid, 382776, 202, 5, 5, true) --awakened-earth
 GridStatusRaidDebuff:DebuffId(zoneid, 381576, 203, 5, 5, true) --seismic-assault
+GridStatusRaidDebuff:DebuffId(zoneid, 382458, 204, 5, 5) -- Resonant Aftermath
+GridStatusRaidDebuff:DebuffId(zoneid, 386352, 205, 6, 6, true) -- Rock Blast
 
 -- "The Primal Council"
 GridStatusRaidDebuff:BossNameId(zoneid, 300, "The Primal Council")
@@ -22,6 +28,9 @@ GridStatusRaidDebuff:DebuffId(zoneid, 371624, 303, 5, 5, true) --conductive-mark
 GridStatusRaidDebuff:DebuffId(zoneid, 372056, 304, 5, 5, true) --crush
 GridStatusRaidDebuff:DebuffId(zoneid, 374792, 305, 5, 5, true) --faultline
 GridStatusRaidDebuff:DebuffId(zoneid, 372027, 306, 5, 5, true) --slashing-blaze
+GridStatusRaidDebuff:DebuffId(zoneid, 375091, 307, 7, 7, true) -- Meteor Axe
+GridStatusRaidDebuff:DebuffId(zoneid, 371836, 308, 5, 5, false, true) -- Primal Blizzard
+GridStatusRaidDebuff:DebuffId(zoneid, 371514, 309, 5, 5) -- Scorched Ground
 
 -- "Sennarth, the Cold Breath"
 GridStatusRaidDebuff:BossNameId(zoneid, 400, "Sennarth, the Cold Breath")
@@ -34,6 +43,8 @@ GridStatusRaidDebuff:DebuffId(zoneid, 372082, 406, 5, 5, true) --enveloping-webs
 GridStatusRaidDebuff:DebuffId(zoneid, 371976, 407, 5, 5, true) --chilling-blast
 GridStatusRaidDebuff:DebuffId(zoneid, 372030, 408, 5, 5, true) --sticky-webbing
 GridStatusRaidDebuff:DebuffId(zoneid, 372055, 409, 5, 5, true) --icy-ground
+GridStatusRaidDebuff:DebuffId(zoneid, 372055, 410, 5, 5) -- Icy Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 373048, 411, 6, 6, true) -- Suffocating Webs
 
 -- "Dathea, Ascended"
 GridStatusRaidDebuff:BossNameId(zoneid, 500, "Dathea, Ascended")
@@ -42,6 +53,9 @@ GridStatusRaidDebuff:DebuffId(zoneid, 377819, 502, 5, 5, true) --lingering-slash
 GridStatusRaidDebuff:DebuffId(zoneid, 374900, 503, 5, 5, true) --microburst
 GridStatusRaidDebuff:DebuffId(zoneid, 376802, 504, 5, 5, true) --razor-winds
 GridStatusRaidDebuff:DebuffId(zoneid, 375580, 505, 5, 5, true) --zephyr-slam
+GridStatusRaidDebuff:DebuffId(zoneid, 390449, 506, 5, 5, true, true) -- Thunderbolt
+GridStatusRaidDebuff:DebuffId(zoneid, 391686, 507, 6, 6, false, true) -- Conductive Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 388290, 508, 5, 5, true) -- Cyclone
 
 -- "Kurog Grimtotem"
 GridStatusRaidDebuff:BossNameId(zoneid, 600, "Kurog Grimtotem")
@@ -54,6 +68,18 @@ GridStatusRaidDebuff:DebuffId(zoneid, 372517, 606, 5, 5, true) --frozen-solid
 GridStatusRaidDebuff:DebuffId(zoneid, 377780, 607, 5, 5, true) --skeletal-fractures
 GridStatusRaidDebuff:DebuffId(zoneid, 374623, 608, 5, 5, true) --frost-binds
 GridStatusRaidDebuff:DebuffId(zoneid, 374554, 609, 5, 5, true) --lava-pool
+GridStatusRaidDebuff:DebuffId(zoneid, 391022, 610, 5, 5, true) -- Frigid Torrent
+GridStatusRaidDebuff:DebuffId(zoneid, 391056, 611, 6, 6) -- Enveloping Earth
+GridStatusRaidDebuff:DebuffId(zoneid, 396109, 612, 5, 5, false, true) -- Frost Dominance
+GridStatusRaidDebuff:DebuffId(zoneid, 396106, 613, 5, 5, false, true) -- Flame Dominance
+GridStatusRaidDebuff:DebuffId(zoneid, 396085, 614, 5, 5, false, true) -- Earth Dominance
+GridStatusRaidDebuff:DebuffId(zoneid, 396113, 615, 5, 5, false, true) -- Storm Dominance
+GridStatusRaidDebuff:DebuffId(zoneid, 374554, 616, 5, 5) -- Magma Pool
+GridStatusRaidDebuff:DebuffId(zoneid, 376063, 617, 5, 5, true) -- Flame Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 391696, 618, 5, 5, true) -- Lethal Current
+GridStatusRaidDebuff:DebuffId(zoneid, 374023, 619, 5, 5, true) -- Searing Carnage
+GridStatusRaidDebuff:DebuffId(zoneid, 372458, 620, 6, 6, true) -- Absolute Zero
+GridStatusRaidDebuff:DebuffId(zoneid, 390920, 621, 5, 5, true) -- Shocking Burst
 
 -- "Broodkeeper Diurna"
 GridStatusRaidDebuff:BossNameId(zoneid, 700, "Broodkeeper Diurna")
@@ -69,9 +95,33 @@ GridStatusRaidDebuff:DebuffId(zoneid, 375653, 709, 5, 5, true) --static-jolt
 GridStatusRaidDebuff:DebuffId(zoneid, 376392, 710, 5, 5, true) --disoriented
 GridStatusRaidDebuff:DebuffId(zoneid, 375876, 711, 5, 5, true) --icy-shards
 GridStatusRaidDebuff:DebuffId(zoneid, 375430, 712, 5, 5, true) --sever-tendon
+GridStatusRaidDebuff:DebuffId(zoneid, 375889, 713, 5, 5) -- Greatstaff's Wrath
+GridStatusRaidDebuff:DebuffId(zoneid, 380483, 714, 5, 5) -- Empowered Greatstaff's Wrath
+GridStatusRaidDebuff:DebuffId(zoneid, 388717, 715, 6, 6) -- Icy Shroud
+GridStatusRaidDebuff:DebuffId(zoneid, 375620, 716, 6, 6, true) -- Ionizing Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 388920, 717, 6, 6) -- Frozen Shroud
+GridStatusRaidDebuff:DebuffId(zoneid, 375487, 718, 6, 6, true) -- Cauterizing Flashflames
+GridStatusRaidDebuff:DebuffId(zoneid, 376260, 719, 5, 5, true, true) -- Tremors
+GridStatusRaidDebuff:DebuffId(zoneid, 375716, 720, 5, 5, true, true) -- Ice Barrage
 
 -- "Raszageth the Storm-Eater"
 GridStatusRaidDebuff:BossNameId(zoneid, 800, "Raszageth the Storm-Eater")
 GridStatusRaidDebuff:DebuffId(zoneid, 381615, 801, 5, 5, true) --static-charge
 GridStatusRaidDebuff:DebuffId(zoneid, 377594, 802, 5, 5, true) --lightning-breath
 GridStatusRaidDebuff:DebuffId(zoneid, 381249, 803, 5, 5, true) --electrifying-presence
+GridStatusRaidDebuff:DebuffId(zoneid, 395906, 804, 5, 5, true) -- Electrified Jaws
+GridStatusRaidDebuff:DebuffId(zoneid, 391285, 805, 6, 6, true, true) -- Thunderstruck Armor
+GridStatusRaidDebuff:DebuffId(zoneid, 388115, 806, 5, 5) -- Lightning Devastation
+GridStatusRaidDebuff:DebuffId(zoneid, 394576, 807, 5, 5, true) -- Positive Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 394579, 808, 5, 5, true) -- Negative Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 394582, 809, 5, 5) -- Focused Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 394583, 810, 6, 6) -- Scattered Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 377467, 811, 5, 5, true) -- Fulminating Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 390911, 812, 5, 5, true) -- Lingering Charge
+GridStatusRaidDebuff:DebuffId(zoneid, 395929, 813, 5, 5) -- Storm's Spite
+GridStatusRaidDebuff:DebuffId(zoneid, 377662, 814, 5, 5) -- Static Field
+GridStatusRaidDebuff:DebuffId(zoneid, 388659, 815, 6, 6, true) -- Tempest Wing
+GridStatusRaidDebuff:DebuffId(zoneid, 377612, 816, 5, 5, true) -- Hurricane Wing
+GridStatusRaidDebuff:DebuffId(zoneid, 381442, 817, 5, 5, true) -- Lightning Strike
+GridStatusRaidDebuff:DebuffId(zoneid, 390763, 818, 7, 7, true) -- Thunderous Energy
+GridStatusRaidDebuff:DebuffId(zoneid, 380573, 819, 5, 5, true) -- Ball Lightning
