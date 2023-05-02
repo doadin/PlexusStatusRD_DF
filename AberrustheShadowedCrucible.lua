a424ad5e1fb1011a8c79c6b8c68c4aeb4890508f
tbkizle <tbkizle@gmail.com>
2023-05-02 15:51:06 -0400

    Initial Support for Aberrus the Shadowed Crucible

---
+++ AberrustheShadowedCrucible.lua
@@ -0,0 +1,95 @@
+-- local zone = "Aberrus, the Shadowed Crucible"
+local zoneid = 2166
+
+-- "Kazzara, the Hellforged"
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Kazzara, the Hellforged")
+GridStatusRaidDebuff:DebuffId(zoneid, 400432, 101, 5, 5, true) --hellbeam
+GridStatusRaidDebuff:DebuffId(zoneid, 403326, 102, 5, 5, true) --wings-of-extinction
+GridStatusRaidDebuff:DebuffId(zoneid, 404744, 103, 5, 5, true) --terror-claws
+GridStatusRaidDebuff:DebuffId(zoneid, 407196, 104, 5, 5, true) --dread-rifts
+
+-- "The Amalgamation Chamber"
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "The Amalgamation Chamber")
+GridStatusRaidDebuff:DebuffId(zoneid, 401809, 201, 5, 5, true) -- corrupting-shadow
+GridStatusRaidDebuff:DebuffId(zoneid, 402617, 202, 5, 5, true) -- blazing-heat
+GridStatusRaidDebuff:DebuffId(zoneid, 406780, 203, 5, 5, true) -- shadowflame-contamination
+GridStatusRaidDebuff:DebuffId(zoneid, 405641, 204, 5, 5, true) -- blistering-twilight
+GridStatusRaidDebuff:DebuffId(zoneid, 408219, 205, 5, 5, true) -- convergent-eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 405914, 206, 5, 5, true) -- withering-vulnerability
+GridStatusRaidDebuff:DebuffId(zoneid, 405016, 207, 5, 5, true) -- umbral-detonation
+
+-- "The Forgotten Experiments"
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "The Forgotten Experiments")
+GridStatusRaidDebuff:DebuffId(zoneid, 406358, 301, 5, 5, true) --rending-charge
+GridStatusRaidDebuff:DebuffId(zoneid, 406305, 302, 5, 5, true) --infused-strikes
+GridStatusRaidDebuff:DebuffId(zoneid, 405042, 303, 5, 5, true) --unstable-essence
+GridStatusRaidDebuff:DebuffId(zoneid, 405383, 304, 5, 5, true) --violent-eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 407617, 305, 5, 5, true) --temporal-anomaly
+GridStatusRaidDebuff:DebuffId(zoneid, 405391, 306, 5, 5, true) --disintegrate
+
+-- "Assault of the Zaqali"
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Assault of the Zaqali")
+GridStatusRaidDebuff:DebuffId(zoneid, 404687, 401, 5, 5, true) --barrier-backfire
+GridStatusRaidDebuff:DebuffId(zoneid, 398938, 402, 5, 5, true) --devastating-leap
+GridStatusRaidDebuff:DebuffId(zoneid, 401258, 403, 5, 5, true) --heavy-cudgel
+GridStatusRaidDebuff:DebuffId(zoneid, 397383, 404, 5, 5, true) --molten-barrier
+GridStatusRaidDebuff:DebuffId(zoneid, 401401, 405, 5, 5, true) --blazing-spear
+GridStatusRaidDebuff:DebuffId(zoneid, 408624, 406, 5, 5, true) --scorching-roar
+GridStatusRaidDebuff:DebuffId(zoneid, 401867, 407, 5, 5, true) --volcanic-shield
+GridStatusRaidDebuff:DebuffId(zoneid, 404687, 408, 5, 5, true) --barrier-backfire
+
+-- "Rashok, the Elder"
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "Rashok, the Elder")
+GridStatusRaidDebuff:DebuffId(zoneid, 405316, 501, 5, 5, true) --ancient-fury
+GridStatusRaidDebuff:DebuffId(zoneid, 406851, 502, 5, 5, true) --doom-flames
+GridStatusRaidDebuff:DebuffId(zoneid, 403543, 503, 5, 5, true) --lava-wave
+GridStatusRaidDebuff:DebuffId(zoneid, 401419, 504, 5, 5, true) --elders-conduit
+
+-- "The Vigilant Steward, Zskarn"
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "The Vigilant Steward, Zskarn")
+GridStatusRaidDebuff:DebuffId(zoneid, 405462, 601, 5, 5, true) --dragonfire-traps
+GridStatusRaidDebuff:DebuffId(zoneid, 404955, 602, 5, 5, true) --shrapnel-bomb
+GridStatusRaidDebuff:DebuffId(zoneid, 404007, 603, 5, 5, true) --unstable-embers
+GridStatusRaidDebuff:DebuffId(zoneid, 403978, 604, 5, 5, true) --blast-wave
+GridStatusRaidDebuff:DebuffId(zoneid, 404942, 605, 5, 5, true) --searing-claws
+
+-- "Magmorax"
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "Magmorax")
+GridStatusRaidDebuff:DebuffId(zoneid, 408359, 701, 5, 5, true) --catastrophic-eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 402989, 702, 5, 5, true) --molten-spittle
+GridStatusRaidDebuff:DebuffId(zoneid, 409853, 703, 5, 5, true) --igniting-roar
+GridStatusRaidDebuff:DebuffId(zoneid, 401348, 704, 5, 5, true) --incinerating-maws
+
+-- "Echo of Neltharion"
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Echo of Neltharion")
+GridStatusRaidDebuff:DebuffId(zoneid, 401022, 801, 5, 5, true) --calamitous-strike
+GridStatusRaidDebuff:DebuffId(zoneid, 403057, 802, 5, 5, true) --surrender-to-corruption
+GridStatusRaidDebuff:DebuffId(zoneid, 407793, 803, 5, 5, true) --sunder-shadow
+GridStatusRaidDebuff:DebuffId(zoneid, 407917, 804, 5, 5, true) --ebon-destruction
+GridStatusRaidDebuff:DebuffId(zoneid, 401825, 805, 5, 5, true) --shatter
+GridStatusRaidDebuff:DebuffId(zoneid, 410972, 806, 5, 5, true) --corruption
+
+-- "Scalecommander Sarkareth"
+GridStatusRaidDebuff:BossNameId(zoneid, 900, "Scalecommander Sarkareth")
+GridStatusRaidDebuff:DebuffId(zoneid, 401951, 901, 5, 5, true) --oblivion
+GridStatusRaidDebuff:DebuffId(zoneid, 401215, 902, 5, 5, true) --emptiness-between-stars
+GridStatusRaidDebuff:DebuffId(zoneid, 413070, 903, 5, 5, true) --destabilize
+GridStatusRaidDebuff:DebuffId(zoneid, 407576, 904, 5, 5, true) --astral-flare
+GridStatusRaidDebuff:DebuffId(zoneid, 401383, 905, 5, 5, true) --oppressing-howl
+GridStatusRaidDebuff:DebuffId(zoneid, 410247, 906, 5, 5, true) --echoing-howl
+GridStatusRaidDebuff:DebuffId(zoneid, 401819, 907, 5, 5, true) --glittering-surge
+GridStatusRaidDebuff:DebuffId(zoneid, 401905, 908, 5, 5, true) --dazzled
+GridStatusRaidDebuff:DebuffId(zoneid, 406989, 909, 5, 5, true) --burning-ground
+GridStatusRaidDebuff:DebuffId(zoneid, 401525, 910, 5, 5, true) --scorching-detonation
+GridStatusRaidDebuff:DebuffId(zoneid, 401718, 911, 5, 5, true) --disintegrated
+GridStatusRaidDebuff:DebuffId(zoneid, 402051, 912, 5, 5, true) --searing-breath
+GridStatusRaidDebuff:DebuffId(zoneid, 402052, 913, 5, 5, true) --seared
+GridStatusRaidDebuff:DebuffId(zoneid, 402746, 914, 5, 5, true) --drifting-embers
+GridStatusRaidDebuff:DebuffId(zoneid, 401325, 915, 5, 5, true) --burning-claws
+GridStatusRaidDebuff:DebuffId(zoneid, 404154, 916, 5, 5, true) --void-surge
+GridStatusRaidDebuff:DebuffId(zoneid, 404218, 917, 5, 5, true) --void-fracture
+GridStatusRaidDebuff:DebuffId(zoneid, 404769, 918, 5, 5, true) --empty-strike
+GridStatusRaidDebuff:DebuffId(zoneid, 404288, 919, 5, 5, true) --infinite-duress
+GridStatusRaidDebuff:DebuffId(zoneid, 404269, 920, 5, 5, true) --ebon-might
+GridStatusRaidDebuff:DebuffId(zoneid, 403497, 921, 5, 5, true) --astral-formation
+GridStatusRaidDebuff:DebuffId(zoneid, 413106, 922, 5, 5, true) --void-might
