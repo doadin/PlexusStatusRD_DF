f598ffbb3ff523ccddf0862d01607217dc7a883b
tbkizle <tbkizle@gmail.com>
2023-11-18 00:33:09 -0500

    Add Amirdrassil the Dreams Hope

---
+++ AmirdrassiltheDreamsHope.lua
@@ -0,0 +1,140 @@
+-- local zone = "Amirdrassil, the Dream's Hope"
+local zoneid = 2232
+
+
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Gnarlroot")
+GridStatusRaidDebuff:DebuffId(zoneid, 421971, 101, 5, 5, true) --Controlled Burn
+GridStatusRaidDebuff:DebuffId(zoneid, 422023, 102, 5, 5, true) --Shadow-Scorched Earth
+GridStatusRaidDebuff:DebuffId(zoneid, 422026, 103, 5, 5, true) --Tortured Scream
+GridStatusRaidDebuff:DebuffId(zoneid, 421038, 104, 5, 5, true) --Ember-Charred		
+GridStatusRaidDebuff:DebuffId(zoneid, 426548, 105, 5, 5, true) --Searing Bramble
+GridStatusRaidDebuff:DebuffId(zoneid, 421840, 106, 5, 5, true) --Uprooted Agony
+GridStatusRaidDebuff:DebuffId(zoneid, 422053, 107, 5, 5, true) --Shadow Spines* 
+GridStatusRaidDebuff:DebuffId(zoneid, 425816, 108, 5, 5, true) --Blazing Pollen*
+GridStatusRaidDebuff:DebuffId(zoneid, 425819, 109, 5, 5, true) --Flaming Sap*
+GridStatusRaidDebuff:DebuffId(zoneid, 424352, 110, 5, 5, true) --Dreadfire Barrage*
+
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Igira the Cruel")
+GridStatusRaidDebuff:DebuffId(zoneid, 414340, 201, 5, 5, true) --Drenched Blades
+GridStatusRaidDebuff:DebuffId(zoneid, 414770, 202, 5, 5, true) --Blistering Torment
+GridStatusRaidDebuff:DebuffId(zoneid, 414367, 203, 5, 5, true) --Gathering Torment	
+GridStatusRaidDebuff:DebuffId(zoneid, 419462, 204, 5, 5, true) --Flesh Mortification
+GridStatusRaidDebuff:DebuffId(zoneid, 423715, 205, 5, 5, true) --Searing Sparks
+GridStatusRaidDebuff:DebuffId(zoneid, 416056, 206, 5, 5, true) --Umbral Destruction
+GridStatusRaidDebuff:DebuffId(zoneid, 429277, 207, 5, 5, true) --Brutalized
+GridStatusRaidDebuff:DebuffId(zoneid, 426017, 208, 5, 5, true) --Vital Rupture
+GridStatusRaidDebuff:DebuffId(zoneid, 422776, 209, 5, 5, true) --Marked for Torment*
+GridStatusRaidDebuff:DebuffId(zoneid, 415624, 210, 5, 5, true) --Heart Stopper*
+GridStatusRaidDebuff:DebuffId(zoneid, 414425, 211, 5, 5, true) --Blistering Spear*
+
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Volcoross")
+GridStatusRaidDebuff:DebuffId(zoneid, 421082, 301, 5, 5, true) --Hellboil
+GridStatusRaidDebuff:DebuffId(zoneid, 427201, 302, 5, 5, true) --Coiling Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 423494, 303, 5, 5, true) --Tidal Blaze
+GridStatusRaidDebuff:DebuffId(zoneid, 419054, 304, 5, 5, true) --Molten Venom
+GridStatusRaidDebuff:DebuffId(zoneid, 421284, 305, 5, 5, true) --Coiling Flames*
+GridStatusRaidDebuff:DebuffId(zoneid, 420934, 306, 5, 5, true) --Flood of the Firelands*
+GridStatusRaidDebuff:DebuffId(zoneid, 421672, 307, 5, 5, true) --Serpent's Fury*
+
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Council of Dreams")
+GridStatusRaidDebuff:DebuffId(zoneid, 420856, 401, 5, 5, true) --Poisonous Javelin
+GridStatusRaidDebuff:DebuffId(zoneid, 421292, 402, 5, 5, true) --Constricting Thicket
+GridStatusRaidDebuff:DebuffId(zoneid, 426390, 403, 5, 5, true) --Corrosive Pollen
+GridStatusRaidDebuff:DebuffId(zoneid, 421032, 404, 5, 5, true) --Captivating Finale
+GridStatusRaidDebuff:DebuffId(zoneid, 418720, 405, 5, 5, true) --Polymorph Bomb
+GridStatusRaidDebuff:DebuffId(zoneid, 427602, 406, 5, 5, true) --Hungry
+GridStatusRaidDebuff:DebuffId(zoneid, 427010, 407, 5, 5, true) --Satiated
+GridStatusRaidDebuff:DebuffId(zoneid, 420409, 408, 5, 5, true) --Quack!
+GridStatusRaidDebuff:DebuffId(zoneid, 424269, 409, 5, 5, true) --Slippery
+GridStatusRaidDebuff:DebuffId(zoneid, 421024, 410, 5, 5, true) --Emerald Winds
+GridStatusRaidDebuff:DebuffId(zoneid, 423551, 411, 5, 5, true) --Whimsical Gust
+GridStatusRaidDebuff:DebuffId(zoneid, 420671, 412, 5, 5, true) --Noxious Blossom*
+GridStatusRaidDebuff:DebuffId(zoneid, 421020, 413, 5, 5, true) --Agonizing Claws*
+GridStatusRaidDebuff:DebuffId(zoneid, 420937, 414, 5, 5, true) --Relentless Barrage*
+GridStatusRaidDebuff:DebuffId(zoneid, 423522, 415, 5, 5, true) --Unstable Venom*
+
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "Larodar, Keeper of the Flame")
+GridStatusRaidDebuff:DebuffId(zoneid, 425889, 501, 5, 5, true) --Igniting Growth
+GridStatusRaidDebuff:DebuffId(zoneid, 425531, 502, 5, 5, true) --Dream Fatigue
+GridStatusRaidDebuff:DebuffId(zoneid, 418522, 503, 5, 5, true) --Blistering Splinters
+GridStatusRaidDebuff:DebuffId(zoneid, 426387, 504, 5, 5, true) --Scorching Bramblethorn
+GridStatusRaidDebuff:DebuffId(zoneid, 419485, 505, 5, 5, true) --Nature's Bulwark+
+GridStatusRaidDebuff:DebuffId(zoneid, 423719, 506, 5, 5, true) --Nature's Fury
+GridStatusRaidDebuff:DebuffId(zoneid, 429032, 507, 5, 5, true) --Everlasting Blaze
+GridStatusRaidDebuff:DebuffId(zoneid, 427306, 508, 5, 5, true) --Encased in Ash
+GridStatusRaidDebuff:DebuffId(zoneid, 427343, 509, 5, 5, true) --Fire Whirl
+GridStatusRaidDebuff:DebuffId(zoneid, 421594, 510, 5, 5, true) --Smoldering Suffocation
+GridStatusRaidDebuff:DebuffId(zoneid, 428946, 511, 5, 5, true) --Ashen Asphyxiation
+GridStatusRaidDebuff:DebuffId(zoneid, 421323, 512, 5, 5, true) --Searing Ash
+
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "Nymue, Weaver of the Cycle")
+GridStatusRaidDebuff:DebuffId(zoneid, 429785, 601, 5, 5, true) --Impending Loom
+GridStatusRaidDebuff:DebuffId(zoneid, 430563, 602, 5, 5, true) --Ephemeral Flora
+GridStatusRaidDebuff:DebuffId(zoneid, 430485, 603, 5, 5, true) --Reclamation
+GridStatusRaidDebuff:DebuffId(zoneid, 425357, 604, 5, 5, true) --Surging Growth
+GridStatusRaidDebuff:DebuffId(zoneid, 420907, 605, 5, 5, true) --Viridian Rain
+GridStatusRaidDebuff:DebuffId(zoneid, 423195, 606, 5, 5, true) --Inflorescence+
+GridStatusRaidDebuff:DebuffId(zoneid, 428273, 607, 5, 5, true) --Woven Resonance?
+GridStatusRaidDebuff:DebuffId(zoneid, 423369, 608, 5, 5, true) --Barrier Blossom
+GridStatusRaidDebuff:DebuffId(zoneid, 421368, 609, 5, 5, true) --Unravel
+GridStatusRaidDebuff:DebuffId(zoneid, 428479, 610, 5, 5, true) --Lucid Vulnerability
+GridStatusRaidDebuff:DebuffId(zoneid, 423842, 611, 5, 5, true) --Verdant Rend
+GridStatusRaidDebuff:DebuffId(zoneid, 418423, 612, 5, 5, true) --Verdant Matrix*
+GridStatusRaidDebuff:DebuffId(zoneid, 420846, 613, 5, 5, true) --Continuum*
+GridStatusRaidDebuff:DebuffId(zoneid, 428012, 614, 5, 5, true) --Lucid Miasma*	
+
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "Smolderon")
+GridStatusRaidDebuff:DebuffId(zoneid, 421656, 701, 5, 5, true) --Cauterizing Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 421643, 702, 5, 5, true) --Emberscar's Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 430325, 703, 5, 5, true) --Inferno
+GridStatusRaidDebuff:DebuffId(zoneid, 421532, 704, 5, 5, true) --Smoldering Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 421858, 705, 5, 5, true) --Ignited Essence
+GridStatusRaidDebuff:DebuffId(zoneid, 421455, 706, 5, 5, true) --Overheated*
+GridStatusRaidDebuff:DebuffId(zoneid, 422577, 707, 5, 5, true) --Searing Aftermath*
+GridStatusRaidDebuff:DebuffId(zoneid, 426018, 708, 5, 5, true) --Seeking Inferno*
+GridStatusRaidDebuff:DebuffId(zoneid, 420950, 709, 5, 5, true) --Blistering Heat*		
+GridStatusRaidDebuff:DebuffId(zoneid, 425574, 710, 5, 5, true) --Lingering Burn*		
+
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Tindral Sageswift, Seer of the Flame")
+GridStatusRaidDebuff:DebuffId(zoneid, 424495, 801, 5, 5, true) --Mass Entanglement
+GridStatusRaidDebuff:DebuffId(zoneid, 424581, 802, 5, 5, true) --Fiery Growth
+GridStatusRaidDebuff:DebuffId(zoneid, 424499, 803, 5, 5, true) --Scorching Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 424582, 804, 5, 5, true) --Lingering Cinder
+GridStatusRaidDebuff:DebuffId(zoneid, 422000, 805, 5, 5, true) --Searing Wrath
+GridStatusRaidDebuff:DebuffId(zoneid, 427297, 806, 5, 5, true) --Flame Surge
+GridStatusRaidDebuff:DebuffId(zoneid, 425657, 807, 5, 5, true) --Fallen Feather
+GridStatusRaidDebuff:DebuffId(zoneid, 429166, 808, 5, 5, true) --Astral Heat
+GridStatusRaidDebuff:DebuffId(zoneid, 424579, 809, 5, 5, true) --Suppressive Ember
+GridStatusRaidDebuff:DebuffId(zoneid, 430583, 810, 5, 5, true) --Germinating Aura
+GridStatusRaidDebuff:DebuffId(zoneid, 424258, 811, 5, 5, true) --Dream Essence+
+GridStatusRaidDebuff:DebuffId(zoneid, 429740, 812, 5, 5, true) --Pulsing Heat+
+GridStatusRaidDebuff:DebuffId(zoneid, 421884, 813, 5, 5, true) --Emerald Gale+
+GridStatusRaidDebuff:DebuffId(zoneid, 422509, 814, 5, 5, true) --Empowered Feather+
+GridStatusRaidDebuff:DebuffId(zoneid, 426687, 815, 5, 5, true) --Poisonous Mushroom*
+GridStatusRaidDebuff:DebuffId(zoneid, 421398, 816, 5, 5, true) --Fire Beam*
+GridStatusRaidDebuff:DebuffId(zoneid, 424665, 817, 5, 5, true) --Seed of Flame*
+GridStatusRaidDebuff:DebuffId(zoneid, 422325, 818, 5, 5, true) --Flaming Tree*
+
+GridStatusRaidDebuff:BossNameId(zoneid, 900, "Fyrakk the Blazing")
+GridStatusRaidDebuff:DebuffId(zoneid, 425483, 901, 5, 5, true) --Incinerated
+GridStatusRaidDebuff:DebuffId(zoneid, 417807, 902, 5, 5, true) --Aflame
+GridStatusRaidDebuff:DebuffId(zoneid, 417443, 903, 5, 5, true) --Fyr'alath's Mark
+GridStatusRaidDebuff:DebuffId(zoneid, 429903, 904, 5, 5, true) --Flamebound
+GridStatusRaidDebuff:DebuffId(zoneid, 429906, 905, 5, 5, true) --Shadowbound
+GridStatusRaidDebuff:DebuffId(zoneid, 419123, 906, 5, 5, true) --Flamefall
+GridStatusRaidDebuff:DebuffId(zoneid, 423601, 907, 5, 5, true) --Seed of Amirdrassil+
+GridStatusRaidDebuff:DebuffId(zoneid, 423717, 908, 5, 5, true) --Bloom+
+GridStatusRaidDebuff:DebuffId(zoneid, 430048, 909, 5, 5, true) --Corrupted Seed
+GridStatusRaidDebuff:DebuffId(zoneid, 430045, 910, 5, 5, true) --Corruption
+GridStatusRaidDebuff:DebuffId(zoneid, 426368, 911, 5, 5, true) --Darkflame Cleave*
+GridStatusRaidDebuff:DebuffId(zoneid, 417455, 912, 5, 5, true) --Dream Rend*
+GridStatusRaidDebuff:DebuffId(zoneid, 410223, 913, 5, 5, true) --Shadowflame Breath*
+GridStatusRaidDebuff:DebuffId(zoneid, 429866, 914, 5, 5, true) --Shadowflame Eruption*
+GridStatusRaidDebuff:DebuffId(zoneid, 422836, 915, 5, 5, true) --Burning Scales*
+GridStatusRaidDebuff:DebuffId(zoneid, 428971, 916, 5, 5, true) --Molten Eruption*
+GridStatusRaidDebuff:DebuffId(zoneid, 428400, 917, 5, 5, true) --Exploding Core*
+GridStatusRaidDebuff:DebuffId(zoneid, 428968, 918, 5, 5, true) --Shadow Cage*
+GridStatusRaidDebuff:DebuffId(zoneid, 422524, 919, 5, 5, true) --Shadowflame Devastation*
+GridStatusRaidDebuff:DebuffId(zoneid, 430051, 920, 5, 5, true) --Searing Screams*
+GridStatusRaidDebuff:DebuffId(zoneid, 422837, 921, 5, 5, true) --Apocalypse Roar*
+GridStatusRaidDebuff:DebuffId(zoneid, 425492, 922, 5, 5, true) --Infernal Maw*
